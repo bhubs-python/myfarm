ea13806adb2ad1f2319913b65ac07d8fcfb77160
Mubarak <mubarak117136@gmail.com>
2018-04-13 12:25:29 +0600

    update db

--- myfarm/settings.py
+++ myfarm/settings.py
@@ -57,16 +57,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'myfarm.wsgi.application'
 
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
 """
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -77,7 +77,6 @@ DATABASES = {
     }
 }
 
-"""
 
 
 
