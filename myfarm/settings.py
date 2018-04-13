dd27c3a3a3967e29c1aec13b1590624b1771706c
Raju Hossen <rmd5111@gmail.com>
2018-04-13 12:51:05 +0600

    pg to mysql change

--- myfarm/settings.py
+++ myfarm/settings.py
@@ -57,27 +57,26 @@ TEMPLATES = [
 WSGI_APPLICATION = 'myfarm.wsgi.application'
 
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
 """
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'myfarm',
+        'NAME': 'my_farm',
         'USER': 'root',
         'PASSWORD': 'root',
         'HOST': 'localhost',
     }
 }
 
-"""
 
 
 
